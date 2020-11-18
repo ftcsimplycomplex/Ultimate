f55c37ff0d8e9f9cc7322af36634706de5b4756c
Arjun Mehan <arjun.mehan1@gmail.com>
2020-11-17 19:54:01 -0500

    Got the right gear box ratio - AM

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDriveByEncoder_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDriveByEncoder_Linear.java
@@ -73,7 +73,7 @@ public class AutoDriveByEncoder_Linear extends LinearOpMode {
     private ElapsedTime     runtime = new ElapsedTime();
 
     static final double     COUNTS_PER_MOTOR_REV    = 537.6 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     DRIVE_GEAR_REDUCTION    = 19.2*0.5 ;     // This is < 1.0 if geared UP
     static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.1415);
