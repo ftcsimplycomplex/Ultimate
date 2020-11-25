b88c7ed4c2059c42a4528ae329f587881deb88fb
Arjun Mehan <arjun.mehan1@gmail.com>
2020-11-25 15:09:52 -0500

    Made DriveTrain class to make it easier to make a Opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class DriveTrain {
+    private OpMode opMode;
+    private HardwareMap hardwareMap;
+    private Telemetry telemetry;
+
+    //Declaring motors
+    private DcMotor leftFront = null;
+    private DcMotor rightFront = null;
+    private DcMotor leftRear = null;
+    private DcMotor rightRear = null;
+    public DriveTrain(){
+        hardwareMap = opMode.hardwareMap;
+        telemetry = opMode.telemetry;
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftFront  = hardwareMap.get(DcMotor.class, "LFD");
+        rightFront = hardwareMap.get(DcMotor.class, "RFD");
+        leftRear  = hardwareMap.get(DcMotor.class, "LRD");
+        rightRear = hardwareMap.get(DcMotor.class, "RRD");
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        leftRear.setDirection(DcMotor.Direction.REVERSE);
+        rightRear.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+}
