5b208650e846358b50283bd6f62823ea7be1b43f
Mahika Kasarabada <mahikakas2008@gmail.com>
2020-12-17 11:46:48 -0500

    Fixed spelling errors and matched class name, to name in code. -MK

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonWobbleAll.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonWobbleAll.java
@@ -62,7 +62,7 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Wobble All", group="Linear Opmode")
+@Autonomous(name="AutonWobbleAll", group="Linear Opmode")
 /*@Disabled*/
 public class AutonWobbleAll extends LinearOpMode {
 
